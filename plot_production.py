34d8363ff145fd186c9d77530dcbf254984f8994
Max Winklhofer <winklh99904@gmail.com>
2024-06-06 13:33:54 +0200

    plot production rate

--- plot_production.py
+++ plot_production.py
@@ -20,30 +20,39 @@ def main(plan: dict):
 
     # create network
     G = nx.DiGraph()
-    for recipe_name in plan:
+    resource_node_names = []
+    manufacturing_node_names = []
+    for recipe_name, rate in plan.items():
         if plan[recipe_name] == 0:
             continue
+        
+        recipe_node_name = f'{recipe_name}\n{round(rate, 2)}/s'
+
         G.add_edges_from([
-            (game.get_bare_item_name(item_name), recipe_name)
+            (game.get_bare_item_name(item_name), recipe_node_name)
             for item_name in game.RECIPES[recipe_name]['ingredients']
         ])
         G.add_edges_from([
-            (recipe_name, game.get_bare_item_name(item_name))
+            (recipe_node_name, game.get_bare_item_name(item_name))
             for item_name in game.RECIPES[recipe_name]['products']
         ])
+
+        if game.RECIPES[recipe_name]['producedIn'] in game.MINING_FACILITIES:
+            resource_node_names.append(recipe_node_name)
+        else:
+            manufacturing_node_names.append(recipe_node_name)
     
     # draw network
-    resource_recipes = [
-        recipe_name
-        for recipe_name in plan
-        if game.RECIPES[recipe_name]['producedIn'] in game.MINING_FACILITIES
-    ]
-    manufacturing_nodes = nx.subgraph(G, 
-        filter(lambda recipe_name: not recipe_name in resource_recipes, plan.keys()))
-    mining_nodes = nx.subgraph(G, 
-        filter(lambda recipe_name: recipe_name in resource_recipes, plan.keys()))
-    item_nodes = nx.subgraph(G, 
-        [node_name for node_name in G.nodes() if not node_name in plan])
+    manufacturing_nodes = nx.subgraph(G,
+        filter(lambda name: name in manufacturing_node_names,
+               G.nodes()))
+    mining_nodes = nx.subgraph(G,
+        filter(lambda name: name in resource_node_names,
+               G.nodes()))
+    recipe_node_names = manufacturing_node_names + resource_node_names
+    item_nodes = nx.subgraph(G,
+        [node_name for node_name in G.nodes()
+         if not node_name in recipe_node_names])
 
     pos = nx.kamada_kawai_layout(G)
     nx.draw_networkx_nodes(G, nodelist=manufacturing_nodes, pos=pos, 

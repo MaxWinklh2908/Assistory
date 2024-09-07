619282d0f4d056dd5e93b8e64adc25cb23c812ec
Max Winklhofer <winklhofer@tufast.de>
2024-09-07 13:29:15 +0200

    read array property

--- io/save_reader.py
+++ io/save_reader.py
@@ -22,7 +22,15 @@ class SaveReader:
             'Int64Property': self._read_int64_property,
             'Int8Property': self._read_int8_property,
             'TextProperty': self._read_text_property,
-    }
+        }
+        
+        self._array_property_parsers = {
+            'ObjectProperty': self._read_array_object_property,
+            'IntProperty': self._read_array_int_property,
+            'StructProperty': self._read_array_struct_property,
+            'ByteProperty': self._read_array_byte_property,
+            'StrProperty': self._read_array_string_property,
+        }
 
     def read_string(self) -> str:
         length = self.read_int() # Including terminating character
@@ -56,16 +64,25 @@ class SaveReader:
         return val
 
     def read_property(self) -> Tuple[str, Union[None, dict]]:
+        original_idx = self.idx
         name = self.read_string()
         if name == '':
             raise ValueError('Invalid name: ' + name)
         if name == 'None':
             return name, None
         val = dict()
+        val['start_idx'] = original_idx
         val['property_type'] = self.read_string()
         if not val['property_type'] in self._property_parsers:
             raise ValueError('Unknown property type: ' + val['property_type'])
-        val.update(self._property_parsers[val['property_type']]())
+        
+        try:
+            val.update(self._property_parsers[val['property_type']]())
+        except Exception as e:
+            print('Reading', val['property_type'], 'failed at original_idx:', original_idx)
+            raise e
+        
+        val['end_idx'] = self.idx
         return name, val
 
     def _read_float_property(self) -> dict:
@@ -100,20 +117,99 @@ class SaveReader:
         index = self.read_int()
         struct_type = self.read_string()
         self.idx += 17 # padding
-        self.idx += n_bytes # TODO
-        return dict()
+        payload = self.read_bytes(n_bytes) # InventoryItem: int, string, int, int
+        return {'struct_type': struct_type, 'payload': payload}
     
-    def _read_array_property(self) -> dict:
-        n_bytes = self.read_int() # after padding
-        index = self.read_int()
+    def _read_array_object_property(self) -> dict:
         val = dict()
-        val['array_type'] = self.read_string()
+        length = self.read_int()
+        val['elements'] = []
+        for _ in range(length):
+            elem = dict()
+            elem['level_name'] = self.read_string()
+            elem['path_name'] = self.read_string()
+            val['elements'].append(elem)
+        return val
+    
+    def _read_array_int_property(self) -> dict:
+        val = dict()
+        length = self.read_int()
+        val['elements'] = []
+        for _ in range(length):
+            val['elements'].append(self.read_int())
+        return val
+    
+    def _read_array_byte_property(self) -> dict:
+        val = dict()
+        length = self.read_int()
+        val['elements'] = self.read_bytes(length)
+        return val
+    
+    def _read_array_string_property(self) -> dict:
+        val = dict()
+        length = self.read_int()
+        val['elements'] = []
+        for _ in range(length):
+            val['elements'].append(self.read_string())
+        return val
+    
+    def _read_array_struct_property(self) -> dict:
+        val = dict()
+        length = self.read_int()
+        name = self.read_string() # duplicate
+        array_property_type = self.read_string()
+        # if not array_property_type == 'StructProperty':
+        #     raise ValueError('Expected StructProperty. Got ' + array_property_type)
+        n_bytes = self.read_int() # after the byte padding (after UUID)
+        padding = self.read_int()
+        val['element_type'] = self.read_string() # SpawnData, LinearColor, InventoryStacks, ScannableResourcePair
+        uuid = [self.read_int(), self.read_int(), self.read_int(), self.read_int()]
         self.idx += 1 # padding
+        
+        # typed data
         original_idx = self.idx
-        # length = self.read_int()
-        self.idx += n_bytes # TODO
+        val['elements'] = []
+        for _ in range(length):
+            if val['element_type'] == 'LinearColor':
+                elem = {
+                    'r': self.read_float(),
+                    'g': self.read_float(),
+                    'b': self.read_float(),
+                    'a': self.read_float()
+                }
+            elif val['element_type'] == 'ScannableResourcePair':
+                elem = dict()
+                elem_name, elem_prop = self.read_property()
+                elem['prop'] = elem_prop
+                elem['name'] = elem_name
+            else:
+                elem = dict()
+                elem_name, elem_prop = self.read_property()
+                elem['prop'] = elem_prop
+                elem['properties'] = self.read_properties()
+                elem['name'] = elem_name
+            val['elements'].append(elem)
+
         if original_idx + n_bytes != self.idx:
             raise ValueError(f'{original_idx} + {n_bytes} != {self.idx}')
+        return val
+    
+    def _read_array_property(self) -> dict:
+        val = dict()
+        n_bytes = self.read_int() # after padding
+        index = self.read_int()
+        val['array_type'] = self.read_string()
+        if not val['array_type'] in self._array_property_parsers:
+            raise ValueError(f' [{self.idx}] Unknown array type: {val["array_type"]}')
+        self.idx += 1 # padding
+        original_idx = self.idx
+        # second part?
+
+        val.update(self._array_property_parsers[val['array_type']]())
+
+        if original_idx + n_bytes != self.idx:
+            raise ValueError(f'{original_idx} + {n_bytes} != {self.idx}')
+        
         return val
     
     def _read_int_property(self) -> dict:
